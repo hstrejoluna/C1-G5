ee96657a357afeb122b91c975adb2e49cc15e9fc
hstrejoluna <trejolunatutoriales@gmail.com>
2021-11-17 15:14:49 -0500

    dotenv y express-session

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const routes = require("./routes");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+require("dotenv").config({ path: "variables.env" });
 
 const app = express();
 
 app.use("/", routes());
 
-app.listen(5000);
+app.use(
+  session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.DATABASE,
+      useNewUrlParser: true,
+    }),
+  })
+);
+
+app.listen(process.env.PORT);
