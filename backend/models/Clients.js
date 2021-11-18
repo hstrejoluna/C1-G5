fa9e97e81bf1e29205450c54ba735ff5a914fd46
hstrejoluna <trejolunatutoriales@gmail.com>
2021-11-18 13:53:53 -0500

    client model done

---
+++ backend/models/Clients.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const clientsSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  department: {
+    type: String,
+    trim: true,
+  },
+  time: {
+    type: String,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model("Clients", clientsSchema);
