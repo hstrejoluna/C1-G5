820e2941395238e8831c5042723b09126a57a267
andrealargo19 <andrealargo@JENNIFERs-MacBook-Pro.local>
2021-11-30 18:24:26 +0100

    agregando la info del servicio temporal pre props

--- components/Plans.js
+++ components/Plans.js
@@ -4,6 +4,46 @@ import iconactive from '../public/assets/icn-circle circle-xs secondary-color-1.
 import iconinactive from '../public/assets/icn-circle circle-xs mute.svg';
 
 function Plans(props){
+    const info_plan = {
+        "name": "Standard",
+        "slogan": "Healthcare at your fingerprints",
+        "price": 50,
+        "services": [
+            {
+                "id":1,
+                "name": "Emergency services 24/7",
+                "active": true
+            }, 
+            {  
+                 "id":2,
+                "name": "Hospitalization services",
+                "active": true
+            },
+            {
+                "id":3,
+                "name": "Basic dental services",
+                "active": true
+            },
+            {   
+                "id":4,
+                "name": "Special procedures",
+                "active": false
+            },
+            {
+                "id":5,
+                "name": "Email and community support",
+                "active": false
+            },
+        ]
+    }
+
+    const service_list = info_plan.services.map( service => 
+        <div key={service.id} className={classes.item}>
+            <Image src={service.active ? iconactive:iconinactive } alt="icon"/>
+            <p>{service.name}</p>
+        </div>
+    )
+
     return(
         <div className={classes.container_plans}>
             <div className={classes.container_title}>
@@ -11,15 +51,17 @@ function Plans(props){
                 <p>Access the best health plans and services and improve your quality of life</p>
             </div>
             <div className={classes.subcontainer}>
-                <h3>STANDARD</h3>
-                <h5>Healthcare at your fingertips</h5>
+                <h3>{info_plan.name}</h3>
+                <h5>{info_plan.slogan}</h5>
                 <div className={classes.container_price}>
-                    <h2>50</h2>
+                    <h2>{info_plan.price}</h2>
                     <p className={classes.price}>$</p>
                     <p className={classes.amount}>Per Month</p>   
                 </div>
                 <div className={classes.container_details}>
-                    <div className={classes.item}>
+                    { service_list }
+                    
+                    {/* <div className={classes.item}>
                         <Image src={iconactive} alt="icon"/>
                         <p>Emergency services 24/7</p>
                     </div>
@@ -38,7 +80,8 @@ function Plans(props){
                     <div className={classes.item}>
                         <Image src={iconinactive} alt="icon"/>
                         <p>Email and community support</p>
-                    </div>                
+                    </div> */}
+
                 </div>
                <button className={classes.button_plans}>Get Plan now</button>      
             </div> 
