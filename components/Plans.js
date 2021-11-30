8ecc5170531def576439400beafc860a3518d934
andrealargo19 <andrealargo@JENNIFERs-MacBook-Pro.local>
2021-11-30 21:11:45 +0100

    elaborando y pasando props componentes plans

--- components/Plans.js
+++ components/Plans.js
@@ -4,40 +4,7 @@ import iconactive from '../public/assets/icn-circle circle-xs secondary-color-1.
 import iconinactive from '../public/assets/icn-circle circle-xs mute.svg';
 
 function Plans(props){
-    const info_plan = {
-        "name": "Standard",
-        "slogan": "Healthcare at your fingerprints",
-        "price": 50,
-        "services": [
-            {
-                "id":1,
-                "name": "Emergency services 24/7",
-                "active": true
-            }, 
-            {  
-                 "id":2,
-                "name": "Hospitalization services",
-                "active": true
-            },
-            {
-                "id":3,
-                "name": "Basic dental services",
-                "active": true
-            },
-            {   
-                "id":4,
-                "name": "Special procedures",
-                "active": false
-            },
-            {
-                "id":5,
-                "name": "Email and community support",
-                "active": false
-            },
-        ]
-    }
-
-    const service_list = info_plan.services.map( service => 
+    const service_list = props.info_services.services.map( service => 
         <div key={service.id} className={classes.item}>
             <Image src={service.active ? iconactive:iconinactive } alt="icon"/>
             <p>{service.name}</p>
@@ -51,37 +18,15 @@ function Plans(props){
                 <p>Access the best health plans and services and improve your quality of life</p>
             </div>
             <div className={classes.subcontainer}>
-                <h3>{info_plan.name}</h3>
-                <h5>{info_plan.slogan}</h5>
+                <h3>{props.info_services.name}</h3>
+                <h5>{props.info_services.slogan}</h5>
                 <div className={classes.container_price}>
-                    <h2>{info_plan.price}</h2>
+                    <h2>{props.info_services.price}</h2>
                     <p className={classes.price}>$</p>
                     <p className={classes.amount}>Per Month</p>   
                 </div>
                 <div className={classes.container_details}>
                     { service_list }
-                    
-                    {/* <div className={classes.item}>
-                        <Image src={iconactive} alt="icon"/>
-                        <p>Emergency services 24/7</p>
-                    </div>
-                    <div className={classes.item}>
-                        <Image src={iconactive} alt="icon"/>
-                        <p>Hospitalization services</p>
-                    </div>
-                    <div className={classes.item}>
-                        <Image src={iconactive} alt="icon"/>
-                        <p>Basic dental care</p>
-                    </div>
-                    <div className={classes.item}>
-                        <Image src={iconinactive} alt="icon"/>
-                        <p>Special procedures</p>
-                    </div>
-                    <div className={classes.item}>
-                        <Image src={iconinactive} alt="icon"/>
-                        <p>Email and community support</p>
-                    </div> */}
-
                 </div>
                <button className={classes.button_plans}>Get Plan now</button>      
             </div> 
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,113 @@ import Head from 'next/head'
 
 
 function PrincipalPage(props){
+  const info_plan = [
+      {
+      "name": "SILVER",
+      "slogan": "Healthcare at your fingerprints",
+      "price": 50,
+      "services": [
+      {
+        "id":1,
+        "name": "Emergency services 24/7",
+        "active": true
+      }, 
+      {  
+        "id":2,
+        "name": "Hospitalization services",
+        "active": true
+      },
+      {
+        "id":3,
+        "name": "Basic dental services",
+        "active": true
+      },
+      {   
+        "id":4,
+        "name": "Special procedures",
+        "active": false
+      },
+      {
+        "id":5,
+        "name": "Email and community support",
+        "active": false
+      },
+    ]
+ }, 
 
+
+
+        {
+          "name": "GOLD",
+          "slogan": "Healthcare at your fingerprints",
+          "price": 50,
+          "services": [
+        {
+          "id":1,
+          "name": "Emergency services 24/7",
+          "active": true
+        }, 
+        {  
+          "id":2,
+          "name": "Hospitalization services",
+          "active": true
+        },
+        {
+          "id":3,
+          "name": "Basic dental services",
+          "active": true
+        },
+        {   
+          "id":4,
+          "name": "Special procedures",
+          "active": false
+        },
+        {
+          "id":5,
+          "name": "Email and community support",
+          "active": false
+        },
+    ]
+  }, 
+
+        {
+          "name": "DIAMOND",
+          "slogan": "Healthcare at your fingerprints",
+          "price": 50,
+          "services": [
+          {
+          "id":1,
+          "name": "Emergency services 24/7",
+          "active": true
+        }, 
+        {  
+          "id":2,
+          "name": "Hospitalization services",
+          "active": true
+        },
+        {
+          "id":3,
+          "name": "Basic dental services",
+          "active": true
+        },
+        {   
+          "id":4,
+          "name": "Special procedures",
+          "active": false
+        },
+        {
+          "id":5,
+          "name": "Email and community support",
+          "active": false
+        },
+    ]
+  }, 
+
+]
+  const list_plans = info_plan.map( plan => 
+     <Plans key={plan.name} info_services={plan}></Plans>
+   ) 
+  
   return (
     <div>
       <Head>
@@ -20,7 +126,7 @@ function PrincipalPage(props){
       <Header></Header>
       <Main></Main>
       <Cards></Cards>
-      <Plans></Plans>
+      {list_plans}
       <Footer></Footer>
       
     </div>
