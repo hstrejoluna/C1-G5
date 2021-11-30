759d11e4ac3a5f16eb3c2e40b89a58fd6f280bb1
andrealargo19 <andrealargo@JENNIFERs-MacBook-Pro.local>
2021-11-30 14:45:17 +0100

    personalización plans

--- components/Plans.js
+++ components/Plans.js
@@ -1,33 +1,47 @@
 import classes from './plans.module.css';
-
+import Image from 'next/image'
+import iconactive from '../public/assets/icn-circle circle-xs secondary-color-1.svg';
+import iconinactive from '../public/assets/icn-circle circle-xs mute.svg';
 
 function Plans(props){
     return(
         <div className={classes.container_plans}>
             <div className={classes.container_title}>
                 <h2>Pricing</h2>
-                <p>Problems trying to resolve 
-                the conflict between the two major 
-                realms of Classical physics: 
-                Newtonian mechanics</p>
+                <p>Access the best health plans and services and improve your quality of life</p>
             </div>
             <div className={classes.subcontainer}>
-                <h3>FREE</h3>
-                <h5>Organize across all 
-                apps by hand</h5>
+                <h3>STANDARD</h3>
+                <h5>Healthcare at your fingertips</h5>
                 <div className={classes.container_price}>
-                    <h2>0</h2>
+                    <h2>50</h2>
                     <p className={classes.price}>$</p>
-                    <p className={classes.amount}>Per Month</p>
+                    <p className={classes.amount}>Per Month</p>   
                 </div>
-                <p>Unlimited product updates</p>
-                <p>Unlimited product updates</p>
-                <p>Unlimited product updates</p>
-                <p>1GB  Cloud storage</p>
-                <p>Email and community support</p>
-                <button>Try for free</button> 
-       </div>
-            
+                <div className={classes.container_details}>
+                    <div className={classes.item}>
+                        <Image src={iconactive} alt="icon"/>
+                        <p>Emergency services 24/7</p>
+                    </div>
+                    <div className={classes.item}>
+                        <Image src={iconactive} alt="icon"/>
+                        <p>Hospitalization services</p>
+                    </div>
+                    <div className={classes.item}>
+                        <Image src={iconactive} alt="icon"/>
+                        <p>Basic dental care</p>
+                    </div>
+                    <div className={classes.item}>
+                        <Image src={iconinactive} alt="icon"/>
+                        <p>Special procedures</p>
+                    </div>
+                    <div className={classes.item}>
+                        <Image src={iconinactive} alt="icon"/>
+                        <p>Email and community support</p>
+                    </div>                
+                </div>
+               <button className={classes.button_plans}>Get Plan now</button>      
+            </div> 
         </div>
     );
 }
