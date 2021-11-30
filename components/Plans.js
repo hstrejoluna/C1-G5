a841a6ecdaa9f4b2db79bcd93d629e90f3a5cd5f
andrealargo19 <andrealargo@JENNIFERs-MacBook-Pro.local>
2021-11-30 10:59:56 +0100

    estructura plans

--- components/Plans.js
+++ components/Plans.js
@@ -4,8 +4,8 @@ import classes from './plans.module.css';
 function Plans(props){
     return(
         <div className={classes.container_plans}>
-            <div>
-            <h2>Pricing</h2>
+            <div className={classes.container_title}>
+                <h2>Pricing</h2>
                 <p>Problems trying to resolve 
                 the conflict between the two major 
                 realms of Classical physics: 
@@ -15,19 +15,17 @@ function Plans(props){
                 <h3>FREE</h3>
                 <h5>Organize across all 
                 apps by hand</h5>
-                    <div>
-                        <h2>0</h2>
-                            <div>
-                                <p>$</p>
-                                <p>Per Month</p>
-                            </div>
+                <div className={classes.container_price}>
+                    <h2>0</h2>
+                    <p className={classes.price}>$</p>
+                    <p className={classes.amount}>Per Month</p>
+                </div>
                 <p>Unlimited product updates</p>
                 <p>Unlimited product updates</p>
                 <p>Unlimited product updates</p>
                 <p>1GB  Cloud storage</p>
                 <p>Email and community support</p>
-                <button>Try for free</button>
-            </div>   
+                <button>Try for free</button> 
        </div>
             
         </div>
