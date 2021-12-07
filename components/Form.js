2baeed6237d645f1d55ebe9f6ca9ac6bef2fc1ed
andrealargo19 <andrealargo@JENNIFERs-MacBook-Pro.local>
2021-12-07 16:42:01 +0100

    actualizando componentes y svgs

--- components/Form.js
+++ components/Form.js
@@ -10,28 +10,28 @@ function Form(props){
                 <div>
                 <h1>BOOK APPOINTMENT</h1>
                 </div>
-                <label for="name">Name*</label>
+                <label htmlFor="name">Name*</label>
                 <input type="text" id="name" name="name" required
-                minlength="4" maxlength="8" size="10" placeholder="Enter First Name"/>
+                minLength="4" maxLength="8" size="10" placeholder="Enter First Name"/>
 
-                <label for="lastname">Last name*</label>
+                <label htmlFor="lastname">Last name*</label>
                 <input type="text" id="lastname" name="lastname" required
-                minlength="4" maxlength="10" size="12" placeholder="Enter Last Name"/>
+                minLength="4" maxLength="10" size="12" placeholder="Enter Last Name"/>
 
-                <label for="birthday">Birthday*</label>
+                <label htmlFor="birthday">Birthday*</label>
                 <input type="date" id="birthday" name="birthday"/>
                 
 
-                <label for="SSN">Social Security Number or ID*</label>
+                <label htmlFor="SSN">Social Security Number or ID*</label>
                 <input type="number" id="SSN" name="SSN" placeholder="Enter Id Number"/>
 
-                <label for="email">Enter your email*</label>
+                <label htmlFor="email">Enter your email*</label>
                 <input type="email" id="email" name="email" placeholder="example@gmail.com"/>
 
-                <label for="phone">Enter phone number*</label>
+                <label htmlFor="phone">Enter phone number*</label>
                 <input type="tel" id="phone" name="phone" placeholder="Enter Phohe Number"/>
 
-                <label for="speciality">Choose a Medical speciality*</label>
+                <label htmlFor="speciality">Choose a Medical speciality*</label>
                 <select id="speciality" name="speciality">
                 <option value="Select">please Select</option>
                 <option value="Cardiology">Cardiology</option>
@@ -40,7 +40,7 @@ function Form(props){
                 <option value="General Medicine">General Medicine</option>
                 </select>
 
-                <label for="professional">Choose a Professional*</label>
+                <label htmlFor="professional">Choose a Professional*</label>
                 <select id="professional" name="professional">
                 <option value="">Please Select</option>
                 <option value="Default">Available</option>
@@ -50,7 +50,7 @@ function Form(props){
                 <option value="Natalia Marín Díaz">Natalia Marín Díaz</option>
                 </select>
 
-                <label for="location">Choose a Location*</label>
+                <label htmlFor="location">Choose a Location*</label>
                 <select id="location" name="location">
                 <option value="">Please Select</option>
                 <option value="El tintal">El tintal</option>
@@ -58,10 +58,10 @@ function Form(props){
                 <option value="Castilla">Castilla</option>
                 </select>
 
-                <label for="reservationtime">Reservation date and time*</label>
+                <label htmlFor="reservationtime">Reservation date and time*</label>
                 <input type="datetime-local" id="reservationtime" name="reservationtime"/>
                 <button className={classes.button_confirm}>
-                <Link href="confirm_data"><a>Make Appointment</a></Link>
+                <Link href="/confirm_data"><a>Make Appointment</a></Link>
                 </button>
             </form>
             
