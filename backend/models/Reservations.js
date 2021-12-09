1e8d0c751756f92ee8e5c22f23eb7fada4eff8cc
hstrejoluna <trejolunatutoriales@gmail.com>
2021-12-09 11:42:27 -0500

    Reservation model apparently done

--- backend/models/Reservations.js
+++ backend/models/Reservations.js
@@ -2,6 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ReservationsSchema = new Schema({
+  client: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  professional: {
+    type: Schema.Types.ObjectId,
+    ref: "professionals",
+    required: true,
+  },
+  specialty: {
+    type: Schema.Types.ObjectId,
+    ref: "specialties",
+    required: true,
+  },
+  location: {
+    type: Schema.Types.ObjectId,
+    ref: "locations",
+    required: true,
+  },
   reservation: {
     day: {
       type: Number,
